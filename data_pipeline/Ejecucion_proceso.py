9b005d3fc2ce877e127f8c19f80680a81ffa21cb
Mateocmcc <mateo.camargo@hotmail.com>
2026-03-27 20:27:26 -0500

    Update Ejecucion_proceso.py

--- data_pipeline/Ejecucion_proceso.py
+++ data_pipeline/Ejecucion_proceso.py
@@ -1,13 +1,17 @@
+from src.Data.datos import run as run_datos
 from src.ingestion.Ingestas import run as run_ingestion
 from src.transformation.silver_transform import run as run_silver
 from src.transformation.gold_transform import run as run_gold
+from src.Visualizacion.query_gold import run as run_Visualizacion
 
 def run_pipeline():
     print("Pipeline iniciado")
 
+    run_datos()
     run_ingestion()
     run_silver()
     run_gold()
+    run_Visualizacion()
 
     print("Pipeline finalizado")
 
